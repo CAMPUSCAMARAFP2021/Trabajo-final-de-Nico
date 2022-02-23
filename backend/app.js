ea8ec4bf1b9876772a152004004d2ff2d2260413
nbustos21 <nicolasbustoos21@gmail.com>
2022-02-23 12:16:09 +0100

    reparing routes and adding things in app.js

--- backend/app.js
+++ backend/app.js
@@ -3,8 +3,15 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors')
 
 var indexRouter = require('./routes/index');
+var autoUsersRouter = require('./routes/autoUsers');
+var allergensRouter = require('./routes/allergens');
+var cookwaresRouter = require('./routes/cookwares');
+var difficultiesRouter = require('./routes/difficulties');
+var ingredientsRouter = require('./routes/ingredients');
+var recipesRouter = require('./routes/recipes');
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -13,6 +20,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -20,7 +28,13 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/autoUsers', autoUsersRouter);
+app.use('/allergens', allergensRouter);
+app.use('/cookwares', cookwaresRouter);
+app.use('/difficulties', difficultiesRouter);
+app.use('/ingredients', ingredientsRouter);
+app.use('/recipes', recipesRouter);
+app.use('/users', usersRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
