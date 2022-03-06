3cf3f025b69eff20ecefa372e745f6ca2871bcab
nbustos21 <nicolasbustoos21@gmail.com>
2022-03-06 12:44:41 +0100

    see and create recipes done

--- react/src/component/Recipe.jsx
+++ react/src/component/Recipe.jsx
@@ -5,11 +5,11 @@ import Card from 'react-bootstrap/Card';
 const Recipe = ({ recipe, onDelete}) =>  
     <Card style={{ width: '20rem' }}>
         <Card.Body>
-            <Card.Title>{recipe.title}</Card.Title>
-            <Card.Title>{recipe.duration}</Card.Title>
-            <Card.Title>{recipe.amount}</Card.Title>
-            <Card.Title>{recipe.description}</Card.Title>
-            <Card.Title>{recipe.difficultuy}</Card.Title>
+            <Card.Title>Título: {recipe.title}</Card.Title>
+            <Card.Title>Duración (en minutos): {recipe.duration}</Card.Title>
+            <Card.Title>Raciones: {recipe.amount}</Card.Title>
+            <Card.Title>Descripción: {recipe.description}</Card.Title>
+            <Card.Title>Dificultad: {recipe.difficulty}</Card.Title>
             <Button variant="primary" onClick={onDelete}>Eliminar</Button>
         </Card.Body>
     </Card>
